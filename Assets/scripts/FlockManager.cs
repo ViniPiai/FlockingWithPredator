b9e7186ac5557224df72b6798d713bf716704cfc
ViniPiai <vinicius-piai@hotmail.com>
2020-07-07 19:14:18 -0300

    Todo o projeto comitado, ainda falta fazer o predador perseguir

---
+++ Assets/scripts/FlockManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+
+    public GameObject fishPrefab;
+    public GameObject sharkPrefab;
+    public int numFish = 20;
+    public GameObject[] allFish;
+    public Vector3 swimLimits = new Vector3(5, 5, 5);
+    public Vector3 goalPos;
+
+    [Header("Fish settings")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed;
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed;
+    [Range(1.0f, 10.0f)]
+    public float neighbourDistance;
+    [Range(0.0f, 5.0f)]
+    public float rotationSpeed;
+
+
+    public bool isFlocking = false;
+
+    void Start()
+    {
+        Vector3 sharkPos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
+                                                                Random.Range(-swimLimits.y, swimLimits.y),
+                                                                Random.Range(-swimLimits.z, swimLimits.z));
+        sharkPrefab = (GameObject)Instantiate(sharkPrefab, sharkPos, Quaternion.identity);
+        sharkPrefab.GetComponent<SharkPredator>().myManager = this;
+        allFish = new GameObject[numFish];
+        for(int i=0;i<numFish;i++)
+        {
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
+                                                                Random.Range(-swimLimits.y, swimLimits.y),
+                                                                Random.Range(-swimLimits.z, swimLimits.z));
+            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
+            allFish[i].GetComponent<Flock>().myManager = this;
+        }
+        goalPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Random.Range(0, 100) < 1)
+            goalPos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
+                                                                Random.Range(-swimLimits.y, swimLimits.y),
+                                                                Random.Range(-swimLimits.z, swimLimits.z));
+    }
+}
---
+++ Assets/scripts/SharkPredator.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkPredator : MonoBehaviour
+{
+    float speed = 5.0f;
+    public FlockManager myManager;
+    bool turning1 = false;
+    // Start is called before the first frame update
+    void Start() { }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Bounds bounds = new Bounds(myManager.transform.position, myManager.swimLimits * 2);
+
+        RaycastHit hit1 = new RaycastHit();
+        Vector3 direction1 = myManager.transform.position - transform.position;
+
+        if (!bounds.Contains(transform.position))
+        {
+            turning1 = true;
+        }
+        else if (Physics.Raycast(transform.position, this.transform.forward * 50, out hit1))
+        {
+            turning1 = true;
+            direction1 = Vector3.Reflect(this.transform.forward, hit1.normal);
+        }
+        else
+        {
+            turning1 = false;
+        }
+
+        if (turning1)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                    Quaternion.LookRotation(direction1), myManager.rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            if (Random.Range(0, 100) < 10)
+                speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
+        }
+
+        transform.Translate(0, 0, Time.deltaTime * speed);
+
+        //if (Random.Range(0, 100) < 10)
+        //    ApplyRules();
+        
+    }
+
+    void ApplyRules()
+    {
+        Vector3 vcentre = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0.01f;
+        int groupSize = 0;
+
+        vcentre = vcentre / groupSize + (myManager.goalPos - this.transform.position);
+        speed = gSpeed / groupSize;
+
+        Vector3 direction = (vcentre + vavoid) - transform.position;
+        if (direction != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                Quaternion.LookRotation(direction), myManager.rotationSpeed * Time.deltaTime);
+        }
+    }
+}
